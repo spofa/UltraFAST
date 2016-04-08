9598f2ad98a883d653c241ef62a30a27b2799a00
Sachin Gupta <sachin-gupta@users.noreply.github.com>
2016-04-08 11:25:31 +0530

    NVNC Code (https://github.com/T1T4N/NVNC) Taken Corrected And Compiled to Run

---
+++ NVNCServer/NVNC Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NVNC;
+
+namespace VNCTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VncServer s = new VncServer("password", 5900, "SACHIN-VNC");
+            try
+            {
+                s.Start();
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
